8f5c4149f22a41536b2f9a08e0b4aa36e3596f29
lakshmilal160-source <lakshmilal160@gmail.com>
2026-08-12 15:55:18 +0530

    changes-mubeena

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -143,7 +143,7 @@
                                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
                             </svg>
                         </span>
-                        <span>Email</span>
+                        <span>{{ $globalSetting->email??"" }}</span>
                     </a>
                 </div>
 
